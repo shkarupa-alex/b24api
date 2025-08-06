f3751f5d258fa890351f3fcb147d82bf15ef0d02
Shkarupa Alex <shkarupa.alex@gmail.com>
2025-08-06 13:51:30 +0300

    Error description fix

--- b24api/error.py
+++ b24api/error.py
@@ -10,11 +10,11 @@ class ApiResponseError(Exception):
 
     def __init__(self, *, code: str, description: str | None, request: str) -> None:
         if code and description:
-            message = f"API error [{code}]: {description} for request {request}"
+            message = f"API error [{code}]: {description} for <Request {request}>"
         elif code:
-            message = f"API error [{code}] for request {request}"
+            message = f"API error [{code}] for <Request {request}>"
         else:
-            message = f"API error: {description} for request {request}"
+            message = f"API error: {description} for <Request {request}>"
         super().__init__(message)
 
 
